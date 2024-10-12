17f21d0caf3691b46dd9f12511ce8548909d77aa
itning <itning@itning.top>
2024-10-12 11:49:47 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import yaml from 'js-yaml';
+import axios from 'axios';
+import { handle } from './modify';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+		const url = new URL(request.url);
+		const path = url.pathname;
+		switch (path) {
+			case '/': {
+				return new Response('up');
+			}
+			case '/json': {
+				const requestUrl = url.searchParams.get('url');
+				if (!requestUrl) {
+					return new Response(null, { status: 400 });
+				}
+				const yamlResponse = await axios.get(requestUrl, {
+					responseType: 'text', timeout: 5000, headers: {
+						'user-agent': 'Clash.for.Windows/0.20.27'
+					}
+				});
+				if (!yamlResponse.data) {
+					return new Response('no response for url', { status: 500 });
+				}
+				const doc = yaml.load(yamlResponse.data);
+				return new Response(JSON.stringify(doc), {
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				});
+			}
+			case '/config': {
+				const requestUrl = url.searchParams.get('url');
+				if (!requestUrl) {
+					return new Response(null, { status: 400 });
+				}
+				const yamlResponse = await axios.get(requestUrl, {
+					responseType: 'text', timeout: 5000, headers: {
+						'user-agent': 'Clash.for.Windows/0.20.27'
+					}
+				});
+				if (!yamlResponse.data) {
+					return new Response('no response for url', { status: 500 });
+				}
+				const doc = yaml.load(yamlResponse.data);
+				const result = handle(doc);
+				const y = yaml.dump(result);
+				return new Response(y, {
+					headers: { 'Content-Type': 'application/yaml' }
+				});
+			}
+			default: {
+				return new Response(null, { status: 404 });
+			}
+		}
+	}
 } satisfies ExportedHandler<Env>;
---
+++ src/modify.ts
@@ -0,0 +1,49 @@
+export function handle(config: any): any {
+    const proxies = config['proxies'] ?? [];
+    const blackProxiesNames = ['時間', '續約', '負載群集'];
+    const proxiesNames = proxies
+        .map((item: { name: any; }) => item.name)
+        .filter((item: string | string[]) => !blackProxiesNames.some(word => item.includes(word)));
+    const commonProxyGroupsConfig = {
+        // 健康检查测试地址
+        url: 'https://www.gstatic.com/generate_204',
+        // 健康检查间隔，如不为 0 则启用定时测试，单位为秒
+        interval: 60 * 10,
+        // 懒惰状态，默认为true,未选择到当前策略组时，不进行测试
+        lazy: true,
+        // 健康检查超时时间，单位为毫秒
+        timeout: 5000,
+        // 最大失败次数，超过则触发一次强制健康检查，默认 5
+        'max-failed-times': 5
+    };
+    const proxyGroups = [
+        {
+            name: "线路选择",
+            type: "select",
+            proxies: proxiesNames,
+            // https://www.clashverge.dev/guide/group_icon/group_icon.html
+            icon: 'https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/adjust.svg',
+            ...commonProxyGroupsConfig
+        },
+        {
+            name: "其他网域",
+            type: "select",
+            proxies: ["线路选择", "DIRECT"],
+            icon: 'https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/fish.svg',
+            ...commonProxyGroupsConfig
+        },
+    ];
+    const rules = [
+        'DOMAIN-SUFFIX,cn,DIRECT',
+        'GEOSITE,CN,DIRECT',
+        'GEOIP,CN,DIRECT,no-resolve',
+        'MATCH,其他网域'
+    ];
+
+    config['dns'] = {
+        "use-system-hosts": true
+    };
+    config['proxy-groups'] = proxyGroups;
+    config['rules'] = rules;
+    return config;
+}
